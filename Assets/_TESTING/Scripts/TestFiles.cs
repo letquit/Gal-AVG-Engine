e0e2d0f5865fc252a8f8769d75003f506e49c76d
letquit <1446444589@qq.com>
2025-09-30 23:45:09 +0800

    Reading Text Files
    读取对话文本

---
+++ Assets/_TESTING/Scripts/TestFiles.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFiles : MonoBehaviour
+{
+    [SerializeField] private TextAsset fileName;
+
+    private void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    IEnumerator Run()
+    {
+        List<string> lines = FileManager.ReadTextAsset(fileName, false);
+
+        foreach (string line in lines)
+            Debug.Log(line);
+        
+        yield return null;
+    }
+}
