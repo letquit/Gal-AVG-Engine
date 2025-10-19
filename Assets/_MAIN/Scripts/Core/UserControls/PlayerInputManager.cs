a5b4de3fa44cbc6b9ae2791345cb6c490bb719c4
letquit <1446444589@qq.com>
2025-10-19 14:30:49 +0800

    Input Manager
    输入管理

--- Assets/_MAIN/Scripts/Core/UserControls/PlayerInputManager.cs
+++ Assets/_MAIN/Scripts/Core/UserControls/PlayerInputManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,22 +10,58 @@ namespace DIALOGUE
     /// </summary>
     public class PlayerInputManager : MonoBehaviour
     {
+        private PlayerInput input;
+
         /// <summary>
-        /// 每帧检查玩家的键盘输入，当检测到空格键或回车键被按下时触发对话继续
+        /// 存储输入动作和对应命令的元组列表
         /// </summary>
-        private void Update()
+        private List<(InputAction action, Action<InputAction.CallbackContext> command)> actions =
+            new List<(InputAction action, Action<InputAction.CallbackContext> command)>();
+
+        /// <summary>
+        /// 在Awake阶段获取PlayerInput组件并初始化输入动作
+        /// </summary>
+        private void Awake()
         {
-            // 检查空格键或回车键是否在当前帧被按下
-            if (Keyboard.current.spaceKey.wasPressedThisFrame || Keyboard.current.enterKey.wasPressedThisFrame)
-                PromptAdvance();
+            input = GetComponent<PlayerInput>();
+
+            InitializeActions();
         }
-        
+
+        /// <summary>
+        /// 初始化输入动作列表，将输入动作与对应的处理函数进行绑定
+        /// </summary>
+        private void InitializeActions()
+        {
+            actions.Add((input.actions["Next"], OnNext));
+        }
+
+        /// <summary>
+        /// 当组件启用时，为所有输入动作注册事件处理函数
+        /// </summary>
+        private void OnEnable()
+        {
+            foreach (var inputAction in actions)
+                inputAction.action.performed += inputAction.command;
+        }
+
+        /// <summary>
+        /// 当组件禁用时，为所有输入动作注销事件处理函数，防止内存泄漏
+        /// </summary>
+        private void OnDisable()
+        {
+            foreach (var inputAction in actions)
+                inputAction.action.performed -= inputAction.command;
+        }
+
         /// <summary>
         /// 触发对话系统继续显示下一个对话内容
         /// </summary>
-        public void PromptAdvance()
+        /// <param name="c">输入动作的回调上下文信息</param>
+        public void OnNext(InputAction.CallbackContext c)
         {
             DialogueSystem.instance.OnUserPrompt_Next();
         }
     }
 }
+
